cc8de5d96fb2881137a778e7aaa506d9f80a9d03
MyungJune <kevinshin123@gmail.com>
2023-08-20 19:01:58 +0900

    edit translation function, remove secret from git

--- Papago_API.py
+++ Papago_API.py
@@ -1,32 +1,58 @@
-import json
-import os
-import sys
-import urllib.request
-
 from config.settings.base import get_secret
 
-NCP_CLIENT_ID = "kdcgbbawp7"
-NCP_CLIENT_SECRET = "ncyq8gM4gtRRjgWy8aKm2Qi3vxzTeEdDFN9eRO1c"
+import requests
 
-def translate_and_extract(sentence, target_language='en'):
+def translate_and_extract(sentence, target_language='영어'):
+    # NCP의 Papago API를 사용해 주어진 한국어 텍스트를 원하는 언어로 번역합니다. 
+    # sentence: 번역하고 싶은 한국어 문장
+    # target_language: 번역 결과 언어 코드
+
+    supported_language = {
+        "영어": "en", 
+        "일본어": "ja", 
+        "중국어 간체": "zh-CN", 
+        "중국어 번체": "zh-TW", 
+        "베트남어": "vi",
+        "태국어": "th", 
+        "인도네시아어": "id", 
+        "프랑스어": "fr", 
+        "스페인어": "es", 
+        "러시아어": "ru",
+        "독일어": "de", 
+        "이탈리아어": "it"
+    }
+
     if not sentence: 
         return ''
     
-    encText = urllib.parse.quote(str(sentence))
-    data = "source=ko&target=en&text=" + encText
-    url = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation"
-    request = urllib.request.Request(url)
-    request.add_header("X-NCP-APIGW-API-KEY-ID",NCP_CLIENT_ID)
-    request.add_header("X-NCP-APIGW-API-KEY",NCP_CLIENT_SECRET)
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-    rescode = response.getcode()
-    if(rescode==200):
-        response_body = response.read()
-        response_json = json.loads(str(response_body.decode('utf-8')))
-        return response_json['message']['result']['translatedText']
-    else:
-        print('Error on translation: ' + rescode)
+    if target_language not in supported_language:
         return sentence
+    
+    lang_code = supported_language[target_language]
+    
+    url = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation"
+    headers = {
+        "X-NCP-APIGW-API-KEY-ID": get_secret('NCP_PAPAGO_CLIENT_ID'),
+        "X-NCP-APIGW-API-KEY":  get_secret('NCP_PAPAGO_CLIENT_SECRET'),
+        "Content-Type": "application/json"
+    }
+    data = {
+        "source": "ko",
+        "target": lang_code,
+        "text": sentence,
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response_status_code = response.status_code
+
+    if response_status_code == 200:
+        translated_text = response.json()['message']['result']['translatedText']
+        return translated_text
+
+    else:
+        print(f"Error: HTTP status code {response_status_code}")
+        print(response.text)
+
     
 
 if __name__ == '__main__':
