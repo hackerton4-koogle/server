e4ee8ad8dbe2fa5e16f9881e250685840a558935
MyungJune <kevinshin123@gmail.com>
2023-09-18 19:54:14 +0900

    Change allowed hosts: server, localhost

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,29 +2,22 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    '*',
+    "223.130.139.5",
+    "127.0.0.1",
 ]
 
-DJANGO_APPS += [
+DJANGO_APPS += []
 
-]
+PROJECT_APPS += []
 
-PROJECT_APPS += [
-
-]
-
-THIRD_PARTY_APPS += [
-    
-]
+THIRD_PARTY_APPS += []
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 # 디폴트 데이터베이스 사용
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-STATIC_ROOT = BASE_DIR / 'static'
